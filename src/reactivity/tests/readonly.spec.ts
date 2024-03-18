88ee9a0af1c5684f241e81a41f5714ea1b2319ff
pauloli <bettercallpaulo@qq.com>
2024-03-18 10:33:31 +0800

    feat：实现 readonly 功能

---
+++ src/reactivity/tests/readonly.spec.ts
@@ -0,0 +1,28 @@
+import { readonly } from "../reative";
+
+describe("readonly", () => {
+  it("happy path", () => {
+    const original = {
+      foo: 1,
+      bar: {
+        baz: 2,
+      },
+    };
+    const wrapped = readonly(original);
+
+    expect(wrapped).not.toBe(original);
+    expect(wrapped.foo).toBe(1);
+  });
+
+  it("warn when call set", () => {
+    console.warn = jest.fn();
+
+    const user = readonly({
+      age: 11,
+    });
+
+    user.age = 12;
+
+    expect(console.warn).toHaveBeenCalled();
+  });
+});
