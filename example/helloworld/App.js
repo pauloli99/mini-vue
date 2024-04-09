c2eb87ac95ce5b7ac7148d2732d1b3a9cf0bdc77
pauloli <bettercallpaulo@qq.com>
2024-04-09 11:06:46 +0800

    22-实现初始化 element 主流程

--- example/helloworld/App.js
+++ example/helloworld/App.js
@@ -3,7 +3,19 @@ import { h } from "../../lib/guide-mini-vue.esm.js";
 export const App = {
   render() {
     // ui
-    return h("div", "hi," + this.msg);
+    return h(
+      "div",
+      {
+        id: "root",
+        class: ["red", "hard"],
+      },
+      // "hi," + this.msg
+      // "hi, mini-vue "
+      [
+        h("span", { class: "red" }, "hi, "),
+        h("span", { class: "green" }, "mini-vue"),
+      ]
+    );
   },
 
   setup() {
--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,4 +1,5 @@
 import { createComponentInstance, setupComponent } from "./component";
+import { isObject } from "../shared";
 
 export function render(vnode, container) {
   // patch => vnode => dom
@@ -7,10 +8,20 @@ export function render(vnode, container) {
 }
 
 function patch(vnode: any, container: any) {
+  console.log(vnode.type);
+
   // TODO 判断vnode是组件还是元素
-  //   如果是元素
-  //   processElement(vnode, container);
-  processComponent(vnode, container);
+  if (typeof vnode.type === "string") {
+    // 如果是元素
+    processElement(vnode, container);
+  } else if (isObject(vnode.type)) {
+    //   如果是元素
+    processComponent(vnode, container);
+  }
+}
+
+function processElement(vnode: any, container: any) {
+  mountElement(vnode, container);
 }
 
 function processComponent(vnode: any, container: any) {
@@ -28,4 +39,32 @@ function setupRenderEffect(instance: any, container: any) {
   const subTree = instance.render();
 
   patch(subTree, container);
+}
+function mountElement(vnode: any, container: any) {
+  const { type, props, children } = vnode;
+
+  const el = document.createElement(type) as HTMLElement;
+
+  if (typeof children === "string") {
+    // children === string
+    el.textContent = children;
+  } else if (Array.isArray(children)) {
+    // children === array
+    mountChildren(vnode, el);
+  }
+
+  // 处理props
+  for (const key in props) {
+    const val = props[key];
+    el.setAttribute(key, val);
+  }
+
+  container.append(el);
+}
+
+function mountChildren(vnode: any, container: any) {
+  const { children } = vnode;
+  children.forEach((vnode) => {
+    patch(vnode, container);
+  });
 }
