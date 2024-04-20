937b3c18a7ce89e6539433ef9ddf2dd0cf2a045b
lichangsong <lichangsong4@huawei.com>
2024-04-20 15:23:13 +0800

    43-实现解析 element 标签

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -1,4 +1,4 @@
-import { NodeTypes } from "./ast";
+import { NodeTypes, TagType } from "./ast";
 
 export function baseParse(content: string) {
   const context = createParserContext(content);
@@ -9,13 +9,40 @@ function parseChildren(context) {
   const nodes: any = [];
 
   let node;
-  if (context.source.startsWith("{{")) {
+  const s = context.source;
+  if (s.startsWith("{{")) {
     node = parseInterpolation(context);
+  } else if (s[0] === "<") {
+    if (/[a-z]/i.test(s[1])) {
+      node = parseElement(context);
+    }
   }
 
   nodes.push(node);
 
   return nodes;
+}
+
+function parseElement(context: any) {
+  const element = parseTag(context, TagType.Start);
+
+  parseTag(context, TagType.End);
+
+  return element;
+}
+
+function parseTag(context: any, type: TagType) {
+  const match: any = /^<\/?([a-z]*)/i.exec(context.source);
+  const tag = match[1];
+  advanceBy(context, match[0].length);
+  advanceBy(context, 1);
+
+  if (type === TagType.End) return;
+
+  return {
+    type: NodeTypes.ELEMENT,
+    tag,
+  };
 }
 
 function parseInterpolation(context) {
